3571fc248e6b634b7df20ed17a69b640eaeebb41
Shantanu-mishra <shantanunihal3@gmail.com>
2022-06-04 13:50:27 +0530

    2nd

--- salary_edition/salary_edition/doctype/additional_components/additional_components.js
+++ salary_edition/salary_edition/doctype/additional_components/additional_components.js
@@ -8,4 +8,13 @@ frappe.ui.form.on('Additional Components', {
 			args:{employee: frm.doc.employee, date:frm.doc.date, overtime_days:frm.doc.overtime_days,incentive_days:frm.doc.incentive_days}
 		})
 	}
+	overtime_days: function(frm){
+	    let otd = frm.doc.overtime_days
+	    if (otd > 6){
+	        let id = otd - 6
+	       frm.set_value('incentive_days',id)
+	       frm.set_value('overtime_days',6)
+	    }
+	    
+	}
 });
