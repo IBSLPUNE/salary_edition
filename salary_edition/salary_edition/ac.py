85f7df74f51d035dab326873c9169138131b115a
Shantanu-mishra <shantanunihal3@gmail.com>
2022-06-05 10:45:17 +0530

    4th

--- salary_edition/salary_edition/ac.py
+++ salary_edition/salary_edition/ac.py
@@ -4,17 +4,19 @@ from erpnext.payroll.doctype.salary_slip.salary_slip import SalarySlip
 @frappe.whitelist()
 def overtime(employee, date,overtime_days,incentive_days):
 	amt = 0.0
+	print(overtime_days)
 	overtime_days = float(overtime_days)
+	print(overtime_days)
 	amt = frappe.get_value('Salary Structure Assignment',{'employee':employee},'base')
 	if amt == None:
 		frappe.throw("Salary Structure Not Assigned To The"+employee)
-	per_day = round((amt/301)*12)
+	per_day = (amt/301)*12
 	ot = round(per_day*(overtime_days*2))
 
 	additional_salary= frappe.get_doc({
 		'doctype': 'Additional Salary',
 		'employee': employee,
-		'salary_component':'OverTime',
+		'salary_component':'Over Time',
 		'type':'Earning',
 		'payroll_date': date,
 		'amount':ot,
